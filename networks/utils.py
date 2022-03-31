610d92d4f1fb4ec1064718da43370cd6372386c9
laurahdezlorenzo <laura.hernandez.lorenzo@gmail.com>
2022-03-31 18:13:05 +0200

    cv models no GNNs

--- networks/utils.py
+++ networks/utils.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 
-def barplot_comparision_others(target, df, metric, title):
+def boxplot_comparision_others(target, df, metric, title):
     
     colors = ["#E76BF3", "#9590ff", "#9590ff", "#9590ff", "#9590ff"]
     custom = sns.set_palette(sns.color_palette(colors))
@@ -11,6 +11,47 @@ def barplot_comparision_others(target, df, metric, title):
     plt.figure(figsize=(8, 8))
     ax = sns.boxplot(x = 'dataset', y = metric, data = df, palette = custom)
                     #  ci = 'sd',
+
+    plt.ylim(0.5, 1.0)
+    plt.xticks(fontsize=16, rotation=40)
+    plt.yticks(fontsize=16)
+    ax.xaxis.label.set_visible(False)
+    plt.ylabel(f'{metric} obtained in test set', fontsize=16)
+    plt.title(title, fontsize=16)
+    plt.tight_layout()
+    plt.show()
+    
+    # plt.savefig(f'figures/figure3{title}.pdf', dpi=500)
+    # plt.savefig(f'figures/figure3{title}.png', dpi=500)
+
+def violinplot_comparision_others(target, df, metric, title):
+    
+    colors = ["#E76BF3", "#9590ff", "#9590ff", "#9590ff", "#9590ff"]
+    custom = sns.set_palette(sns.color_palette(colors))
+
+    plt.figure(figsize=(12, 8))
+    ax = sns.violinplot(x = 'dataset', y = metric, data = df, palette = custom)
+    ax = sns.swarmplot(x = 'dataset', y = metric, data = df, palette = custom, color='white')
+
+    plt.ylim(0.2, 1.0)
+    plt.xticks(fontsize=16, rotation=40)
+    plt.yticks(fontsize=16)
+    ax.xaxis.label.set_visible(False)
+    plt.ylabel(f'{metric} obtained in test set', fontsize=16)
+    plt.title(title, fontsize=16)
+    plt.tight_layout()
+    plt.show()
+    
+    # plt.savefig(f'figures/figure3{title}.pdf', dpi=500)
+    # plt.savefig(f'figures/figure3{title}.png', dpi=500)
+
+def barplot_comparision_others(target, df, metric, title):
+    
+    colors = ["#E76BF3", "#9590ff", "#9590ff", "#9590ff", "#9590ff"]
+    custom = sns.set_palette(sns.color_palette(colors))
+
+    plt.figure(figsize=(8, 8))
+    ax = sns.barplot(x = 'dataset', y = metric, data = df, palette = custom, ci = 'sd')
 
     plt.ylim(0.5, 1.0)
     plt.xticks(fontsize=16, rotation=40)
