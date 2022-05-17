d9302b33441404f44873a802fbc7f4cb2279f0b6
laurahdezlorenzo <laura.hernandez.lorenzo@gmail.com>
2022-05-17 14:44:40 +0200

    resolve last suggestions

--- ml_models/models_stats.py
+++ ml_models/models_stats.py
@@ -11,8 +11,6 @@ def pvalues_nonGNNs(df):
     pvalues_ran = {}
 
     bas = df.loc[df['model'] == 'Baseline model']['auc'].values
-    
-    ran = [0.5]*10
 
     for d in ['AD PPT-Ohmnet', 'AD PPT-Ohmnet no APOE']: 
         for m in ['Logistic Regression', 'SVM Linear', 'SVM RBF', 'Random Forest', 'GNN GraphGym']: # when GG results no APOE
@@ -20,24 +18,20 @@ def pvalues_nonGNNs(df):
             tmp = df.loc[(df['dataset'] == d) & (df['model'] == m)]['auc'].values
             gnn = df.loc[(df['dataset'] == d) & (df['model'] == 'GNN GraphGym')]['auc'].values
 #             print(d, m)
-#             print(tmp)
-#             print(gnn)
+#             print(tmp.mean())
+#             print(gnn.mean())
 #             print()
             t_bas, pval_bas = stats.ttest_ind(tmp, bas, alternative='greater')
             t_gnn, pval_gnn = stats.ttest_ind(tmp, gnn, alternative='less')
-            t_ran, pval_ran = stats.ttest_ind(tmp, ran, alternative='greater')
+            t_ran, pval_ran = stats.ttest_1samp(tmp, 0.5, alternative='less')
             
             pvalues_bas[f'{d} - {m}'] = pval_bas
             pvalues_gnn[f'{d} - {m}'] = pval_gnn
             pvalues_ran[f'{d} - {m}'] = pval_ran
 
-        pvalues_bas_sorted = {k: v for k, v in sorted(pvalues_bas.items(), key=lambda item: item[1])}
-        pvalues_gnn_sorted = {k: v for k, v in sorted(pvalues_gnn.items(), key=lambda item: item[1])}
-        pvalues_ran_sorted = {k: v for k, v in sorted(pvalues_ran.items(), key=lambda item: item[1])}
-
     print('Against baseline:')
-    for k in pvalues_bas_sorted:
-        p = pvalues_bas_sorted[k]
+    for k in pvalues_bas:
+        p = pvalues_bas[k]
         if p < 0.05:
             print('(*)', '{:0.4e}'.format(p), k)
         else:
@@ -45,8 +39,8 @@ def pvalues_nonGNNs(df):
     print()
     
     print('Against GNN:')
-    for k in pvalues_gnn_sorted:
-        p = pvalues_gnn_sorted[k]
+    for k in pvalues_gnn:
+        p = pvalues_gnn[k]
         if p < 0.05:
             print('(*)', '{:0.4e}'.format(p), k)
         else:
@@ -54,8 +48,8 @@ def pvalues_nonGNNs(df):
     print()
 
     print('Against random:')
-    for k in pvalues_ran_sorted:
-        p = pvalues_ran_sorted[k]
+    for k in pvalues_ran:
+        p = pvalues_ran[k]
         if p < 0.05:
             print('(*)', '{:0.4e}'.format(p), k)
         else:
