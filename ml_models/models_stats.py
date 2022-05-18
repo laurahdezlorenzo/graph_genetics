aab106d9cdb98460337634d2226fb91021b9fedb
laurahdezlorenzo <laura.hernandez.lorenzo@gmail.com>
2022-05-18 12:32:22 +0200

    repo cleaning

--- ml_models/models_stats.py
+++ ml_models/models_stats.py
@@ -9,11 +9,12 @@ def pvalues_nonGNNs(df):
     pvalues_bas = {}
     pvalues_gnn = {}
     pvalues_ran = {}
+    pvalues_apoe = {}
 
     bas = df.loc[df['model'] == 'Baseline model']['auc'].values
 
-    for d in ['AD PPT-Ohmnet', 'AD PPT-Ohmnet no APOE']: 
-        for m in ['Logistic Regression', 'SVM Linear', 'SVM RBF', 'Random Forest', 'GNN GraphGym']: # when GG results no APOE
+    for d in ['AD PPT-Ohmnet', 'AD PPT-Ohmnet no APOE']:        
+        for m in ['Logistic Regression', 'SVM Linear', 'SVM RBF', 'Random Forest', 'GNN GraphGym']:
             
             tmp = df.loc[(df['dataset'] == d) & (df['model'] == m)]['auc'].values
             gnn = df.loc[(df['dataset'] == d) & (df['model'] == 'GNN GraphGym')]['auc'].values
@@ -28,6 +29,15 @@ def pvalues_nonGNNs(df):
             pvalues_bas[f'{d} - {m}'] = pval_bas
             pvalues_gnn[f'{d} - {m}'] = pval_gnn
             pvalues_ran[f'{d} - {m}'] = pval_ran
+    
+    for m in ['Logistic Regression', 'SVM Linear', 'SVM RBF', 'Random Forest', 'GNN GraphGym']:
+        
+        apoe    = df.loc[(df['dataset'] == 'AD PPT-Ohmnet')  & (df['model'] == m)]['auc'].values
+        no_apoe = df.loc[(df['dataset'] == 'AD PPT-Ohmnet no APOE')  & (df['model'] == m)]['auc'].values
+        
+        t_apoe, pval_apoe = stats.ttest_ind(no_apoe, apoe, alternative='less')
+        pvalues_apoe[f'{m}'] = pval_apoe
+        
 
     print('Against baseline:')
     for k in pvalues_bas:
@@ -50,6 +60,16 @@ def pvalues_nonGNNs(df):
     print('Against random:')
     for k in pvalues_ran:
         p = pvalues_ran[k]
+        if p < 0.05:
+            print('(*)', '{:0.4e}'.format(p), k)
+        else:
+            print('( )', '{:0.4e}'.format(p), k)
+    
+    print()
+    
+    print('With APOE vs. without APOE:')
+    for k in pvalues_apoe:
+        p = pvalues_apoe[k]
         if p < 0.05:
             print('(*)', '{:0.4e}'.format(p), k)
         else:
