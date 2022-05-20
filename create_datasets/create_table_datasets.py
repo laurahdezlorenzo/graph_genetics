6f104215e876ae3157b260a2bb3afb99b2fe7e03
laurahdezlorenzo <36201993+laurahdezlorenzo@users.noreply.github.com>
2022-05-20 10:51:50 +0200

    Update create_table_datasets.py

--- create_datasets/create_table_datasets.py
+++ create_datasets/create_table_datasets.py
@@ -24,45 +24,6 @@ def adni_data(mode, best_network, data_file, metadata_file):
         result = pd.concat([data, metadata], axis=1)
         return result
 
-    # # For variants
-    # elif mode == 'variants':
-
-    #     variant_types = ['missense_variant',
-	# 			'synonymous_variant',
-	# 			'stop_gained',
-	# 			'frameshift_variant', 
-	# 			'inframe_deletion',
-	# 			'inframe_insertion',
-	# 			'stop_retained_variant', 
-	# 			'coding_sequence_variant',
-	# 			'start_lost',
-	# 			'stop_lost',
-	# 			'protein_altering_variant',
-	# 			'3_prime_utr', '5_prime_utr',
-	# 			'non_coding_transcript_exon_variant',
-	# 			'non_coding_transcript_variant',
-	# 			'upstream_gene_variant',
-	# 			'downstream_gene_variant',
-	# 			'intron_variant',
-	# 			'splice_region_variant']
-        
-    #     gene_dfs = []
-    #     for col in data.columns:
-    #         gene_data = pd.DataFrame(data[col].str.split(', ', 19).tolist(),
-    #                                     columns = variant_types,
-    #                                     index=data.index)
-            
-            
-    #         gene_data['missense_variant'].replace('\[', '', regex=True, inplace=True)
-    #         gene_data['splice_region_variant'].replace('\]', '', regex=True, inplace=True)
-    #         gene_data = gene_data.add_prefix(f'{col}_')
-    #         gene_dfs.append(gene_data)
-
-    #     new_data = pd.concat(gene_dfs, axis=1)
-    #     result = pd.concat([new_data, metadata], axis=1)
-
-    #     return result
-
 def load_data(mode, disease, data_file, metadata_file):
 
     data = pd.read_csv(data_file, index_col=0)
